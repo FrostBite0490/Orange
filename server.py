06db1114cec516092d3eccbe26d408be325e6ab7
Kamal Suresh <kamalsuresh03@gmail.com>
2022-02-12 23:00:28 +0530

    4th commit

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ import sys
 import sort
 
 SECRET_KEY = os.urandom(32)
-global sup
+gaming,batt,disp,cam,price=0,0,0,0,0
 
 try:
     conn=mariadb.connect(
@@ -43,8 +43,8 @@ def read_maria():
             'price':price.__float__()}
         #print(phonedata)
         phonelist.append(phonedata)
+        return phonelist
     #print(phonelist)
-    sort.analyze(phonelist)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 @app.route("/")
@@ -55,10 +55,55 @@ def hello_world():
 @app.route('/2',methods=['POST','GET'])
 def r2():
     if request.method=='POST':
-        #val=request.form.get("1")
-        #print(val)
+        global gaming
+        gaming=float(request.form['rating'])
+        print(type(gaming))
+        print("-------",gaming)
         return render_template("Rating2.html")    
 
+@app.route('/3',methods=['POST','GET'])
+def r3():
+    if request.method=='POST':
+        global batt
+        batt=float(request.form['rating'])
+        print("-------",batt)
+        return render_template("Rating3.html") 
+
+@app.route('/4',methods=['POST','GET'])
+def r4():
+    if request.method=='POST':
+        global disp
+        disp=float(request.form['rating'])
+        print("-------",disp)
+        return render_template("Rating4.html") 
+
+@app.route('/5',methods=['POST','GET'])
+def r5():
+    if request.method=='POST':
+        global cam
+        cam=float(request.form['rating'])
+        print("-------",cam)
+        return render_template("Rating5.html") 
+
+@app.route('/f',methods=['POST','GET'])
+def final():
+    if request.method=='POST':
+        global gaming,batt,disp,cam
+        price=float(request.form['rating'])
+        print("-------",price)
+        phonelist=read_maria()
+        user_pref={
+            'gaming':gaming,
+            'batt':batt,
+            'disp':disp,
+            'cam':cam,
+            'price':price,
+            'name':''}
+        phone=sort.analyze(phonelist,user_pref)
+        print(phone)
+        return render_template("final.html",data=phone) 
+        
+print(cam) 
 #read_maria()
 if __name__ == '__main__':
        app.run(debug=True, host='0.0.0.0')
--- sort.py
+++ sort.py
@@ -1,47 +1,48 @@
 from codecs import ignore_errors
 from unicodedata import name
 import pandas as pd
-def analyze():
+def analyze(phone_list,user_pref):
     #creating a dataframe to store phone values and aggregate 
-    panda_agg=pd.DataFrame(columns=['gaming','batt','disp','cam','name','agg','sup']) 
+    pagg=pd.DataFrame(columns=['gaming','batt','disp','cam','name','price','agg','sup']) 
     agg_list=[]
-    phone_list=[    #Sample phone database. temp solution, should replace with actual db
-        {'gaming':4,
-         'batt':9,
-         'disp':6,
-         'cam':4,
-         'name':'a'
-        },
-        {'gaming':9,
-         'batt':6,
-         'disp':9,
-         'cam':8,
-         'name':'b'
-        },
-        {'gaming':6,
-         'batt':8,
-         'disp':7,
-         'cam':8,
-         'name':'c'
-        },
-        {'gaming':1,
-         'batt':2,
-         'disp':10,
-         'cam':5,
-         'name':'d'
-        },
-        {'gaming':6,
-         'batt':8,
-         'disp':7,
-         'cam':4,
-         'name':'e'
-        },
-    ]
-    user_pref={'gaming':6,  #user preferences
-                'batt':7,
-                'disp':8,
-                'cam':9,
-                'name':''}
+    #phone_list=[    #Sample phone database. temp solution, should replace with actual db
+    #    {'gaming':4,
+    #     'batt':9,
+    #     'disp':6,
+    #     'cam':4,
+    #     'name':'a'
+    #    },
+    #    {'gaming':9,
+    #     'batt':6,
+    #     'disp':9,
+    #     'cam':8,
+    #     'name':'b'
+    #    },
+    #    {'gaming':6,
+    #     'batt':8,
+    #     'disp':7,
+    #     'cam':8,
+    #     'name':'c'
+    #    },
+    #    {'gaming':1,
+    #     'batt':2,
+    #     'disp':10,
+    #     'cam':5,
+    #     'name':'d'
+    #    },
+    #    {'gaming':6,
+    #     'batt':8,
+    #     'disp':7,
+    #     'cam':4,
+    #     'name':'e'
+    #    },
+    #]
+    #user_pref={'gaming':6,  #user preferences
+    #            'batt':7,
+    #            'disp':8,
+    #            'cam':9,
+    #            
+    #            'name':''}
     for item in range(len(phone_list)):
         sup=0
         disp,gaming,batt,cam=0,0,0,0
@@ -51,48 +52,47 @@ def analyze():
             disp=abs(user_pref['disp']-phone_data['disp'])
             sup=0
         else:
-            disp+=0
+            disp=0
             sup+=phone_data['disp']-user_pref['disp']       #supplementary value
             
         if (user_pref['gaming']>phone_data['gaming']):
             gaming=abs(user_pref['gaming']-phone_data['gaming'])
         else:
-            gaming+=0
+            gaming=0
             sup+= phone_data['gaming']-user_pref['gaming']
             
         if (user_pref['batt']>phone_data['batt']):
             batt=abs(phone_data['batt']-user_pref['batt'])
         else:
-            batt+=0
-
+            batt=0
             sup+= phone_data['batt']-user_pref['batt']
         
         if (user_pref['cam']>phone_data['cam']):
             cam=abs(user_pref['cam']-phone_data['cam'])
         else:
-            cam+=0
+            cam=0
             sup= phone_data['cam']-user_pref['cam']
         
         agg=disp+gaming+batt+cam            #adding the all values to get aggregate
        # print(batt,agg,phone_data['name'])
-        panda_agg=panda_agg.append({        #adding values to the dataframe
+        pagg=pagg.append({        #adding values to the dataframe
             'gaming':phone_data['gaming'],
             'batt':phone_data['batt'],
             'disp':phone_data['disp'],
             'cam':phone_data['cam'],
             'name':phone_data['name'],
+            'price':phone_data['price'],
             'agg':agg,
             'sup':sup
             },ignore_index=True)
     
-    panda_agg=panda_agg.sort_values(by=['agg'],ascending=True)
-    
-    if(len(panda_agg.loc[panda_agg['agg']==panda_agg['agg'].iloc[0]])==1):
+    pagg=pagg.sort_values(by=['agg'],ascending=True)
+    if(len(pagg.loc[pagg['agg']==pagg['agg'].iloc[0]])==1):
         print("hello")
+        winner=pagg.iloc[0].to_dict()
     else:
-        pf=panda_agg.loc[panda_agg['agg']==panda_agg['agg'].iloc[0]]
+        pf=pagg.loc[pagg['agg']==pagg['agg'].iloc[0]]
         pf=pf.sort_values(by=['sup'],ascending=False)
         winner=pf.iloc[0].to_dict()
-    print(winner)
+   # print(winner)
     return winner
-analyze()
