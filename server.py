dad3c2681899fd1cbbf0ade51eb70604d7c7bfe2
Kamal Suresh <kamalsuresh03@gmail.com>
2022-02-13 11:01:25 +0530

    6th commit

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from re import T
 from unicodedata import name
 from unittest import expectedFailure, result
 import mariadb
-from flask import Flask,render_template,request
+from flask import Flask,render_template, render_template_string,request
 from markupsafe import re
 #from forms import UserForm
 import os
@@ -58,54 +58,63 @@ def r2():
    
     #if(request.form['rating']):
     gaming=float(request.form['rating'])
-    if gaming!=None:
-        #print(type(gaming))
-        #print("-------",gaming)
+    if gaming!=0:
         return render_template("Rating2.html")
     else:
         print('Noooooo')
-        #return render_template("Rating.html",data="Select A Value")
+        return render_template("Rating.html",q='Gaming',data="Select A Value")
 @app.route('/3',methods=['POST','GET'])
 def r3():
     if request.method=='POST':
         global batt
         batt=float(request.form['rating'])
-        print("-------",batt)
-        return render_template("Rating3.html") 
-
+        if batt!=0:
+            print("-------",batt)
+            return render_template("Rating3.html") 
+        else:
+            return render_template("Rating2.html",data="Select")
 @app.route('/4',methods=['POST','GET'])
 def r4():
     if request.method=='POST':
         global disp
         disp=float(request.form['rating'])
-        print("-------",disp)
-        return render_template("Rating4.html") 
+        if disp!=0:
+            print("-------",disp)
+            return render_template("Rating4.html") 
+        else:
+            return render_template("Rating3.html",data="Select")
 
 @app.route('/5',methods=['POST','GET'])
 def r5():
     if request.method=='POST':
         global cam
         cam=float(request.form['rating'])
-        print("-------",cam)
-        return render_template("Rating5.html") 
+        if cam!=0:
+            print("-------",cam)
+            return render_template("Rating5.html") 
+        else:
+            return render_template("Rating4.html",data="select")
 
 @app.route('/f',methods=['POST','GET'])
 def final():
     if request.method=='POST':
         global gaming,batt,disp,cam
         price=float(request.form['rating'])
-        print("-------",price)
-        phonelist=read_maria()
-        user_pref={
-            'gaming':gaming,
-            'batt':batt,
-            'disp':disp,
-            'cam':cam,
-            'price':price,
-            'name':''}
-        phone=sort.analyze(phonelist,user_pref)
-        print(phonelist)
-        return render_template("final.html",data=phone) 
+        if price!=0:
+            print("-------",price)
+            phonelist=read_maria()
+            user_pref={
+                'gaming':gaming,
+                'batt':batt,
+                'disp':disp,
+                'cam':cam,
+                'price':price,
+                'name':''}
+            phone=sort.analyze(phonelist,user_pref)
+            print(phonelist)
+            return render_template("final.html",data=phone) 
+        else:
+            return render_template("Rating5.html",data="Select")
         
 print(cam) 
 #read_maria()
