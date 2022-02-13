61ec0667d6e515b645179c9470c9041ab8055589
Kamal Suresh <kamalsuresh03@gmail.com>
2022-02-13 13:34:09 +0530

    7th commit

---
+++ orange/server.py
@@ -0,0 +1,127 @@
+
+from crypt import methods
+from dis import dis
+from re import T
+from unicodedata import name
+from unittest import expectedFailure, result
+import mariadb
+from flask import Flask,render_template, render_template_string,request
+from markupsafe import re
+#from forms import UserForm
+import os
+import csv
+import pandas as pd
+#from sqlalchemy import false, null, true
+import sys
+import sort
+
+SECRET_KEY = os.urandom(32)
+gaming,batt,disp,cam,price=0,0,0,0,0
+
+try:
+    conn=mariadb.connect(
+        user="kamal",
+        password="k1",
+        host='localhost',
+        database='orange'
+    )
+except mariadb.Error as e:
+    print(e)
+    sys.exit(1) 
+    
+def read_maria():    
+    phonelist=[]
+    cur = conn.cursor()
+    cur.execute("SELECT* FROM phonelist")
+    for(name,performance,display,battery,camera,price) in cur:
+        phonedata={
+            'gaming':performance.__float__(),
+            'batt':battery.__float__(),
+            'disp':display.__float__(),
+            'cam':camera.__float__(),
+            'name':name.__str__(),
+            'price':price.__float__()}
+        phonelist.append(phonedata)
+        #print(phonedata)
+    #print(phonelist)        
+    return phonelist
+    
+app = Flask(__name__ ,static_folder="/static")
+app.config['SECRET_KEY'] = SECRET_KEY
+@app.route("/")
+def hello_world():
+    data='a'
+    return render_template('index.html',data=data)
+
+@app.route("/1",methods=['POST','GET'])
+def r1():
+    data='a'
+    return render_template('Rating.html')
+
+@app.route('/2',methods=['POST','GET'])
+def r2():
+   
+    #if(request.form['rating']):
+    gaming=float(request.form['rating'])
+    if gaming!=0:
+        return render_template("Rating2.html")
+    else:
+        print('Noooooo')
+        return render_template("Rating.html",q='Gaming',data="Select A Value")
+@app.route('/3',methods=['POST','GET'])
+def r3():
+    if request.method=='POST':
+        global batt
+        batt=float(request.form['rating'])
+        if batt!=0:
+            print("-------",batt)
+            return render_template("Rating3.html") 
+        else:
+            return render_template("Rating2.html",data="Select a value")
+@app.route('/4',methods=['POST','GET'])
+def r4():
+    if request.method=='POST':
+        global disp
+        disp=float(request.form['rating'])
+        if disp!=0:
+            print("-------",disp)
+            return render_template("Rating4.html") 
+        else:
+            return render_template("Rating3.html",data="Select a value")
+
+@app.route('/5',methods=['POST','GET'])
+def r5():
+    if request.method=='POST':
+        global cam
+        cam=float(request.form['rating'])
+        if cam!=0:
+            print("-------",cam)
+            return render_template("Rating5.html") 
+        else:
+            return render_template("Rating4.html",data="Select a value")
+
+@app.route('/f',methods=['POST','GET'])
+def final():
+    if request.method=='POST':
+        global gaming,batt,disp,cam
+        price=float(request.form['rating'])
+        if price!=0:
+            print("-------",price)
+            phonelist=read_maria()
+            user_pref={
+                'gaming':gaming,
+                'batt':batt,
+                'disp':disp,
+                'cam':cam,
+                'price':price,
+                'name':''}
+            phone=sort.analyze(phonelist,user_pref)
+            print(phonelist)
+            return render_template("final.html",data=phone) 
+        else:
+            return render_template("Rating5.html",data="Select")
+        
+print(cam) 
+#read_maria()
+if __name__ == '__main__':
+       app.run(debug=True, host='0.0.0.0')
---
+++ orange/sort.py
@@ -0,0 +1,124 @@
+from codecs import ignore_errors
+from unicodedata import name
+import pandas as pd
+def analyze(phone_list,user_pref):
+    #creating a dataframe to store phone values and aggregate 
+    pagg=pd.DataFrame(columns=['gaming','batt','disp','cam','name','price','agg','sup']) 
+    agg_list=[]
+    #phone_list=[    #Sample phone database. temp solution, should replace with actual db
+    #    {'gaming':4,
+    #     'batt':9,
+    #     'disp':6,
+    #     'cam':4,
+    #     'name':'a'
+    #    },
+    #    {'gaming':9,
+    #     'batt':6,
+    #     'disp':9,
+    #     'cam':8,
+    #     'name':'b'
+    #    },
+    #    {'gaming':6,
+    #     'batt':8,
+    #     'disp':7,
+    #     'cam':8,
+    #     'name':'c'
+    #    },
+    #    {'gaming':1,
+    #     'batt':2,
+    #     'disp':10,
+    #     'cam':5,
+    #     'name':'d'
+    #    },
+    #    {'gaming':6,
+    #     'batt':8,
+    #     'disp':7,
+    #     'cam':4,
+    #     'name':'e'
+    #    },
+    #]
+    #user_pref={'gaming':6,  #user preferences
+    #            'batt':7,
+    #            'disp':8,
+    #            'cam':9,
+    #            
+    #            'name':''}
+    for item in range(len(phone_list)):
+        sup=0
+        disp,gaming,batt,cam=0,0,0,0
+        #comparing user and sample phone data and subtracting 
+        phone_data=phone_list[item]
+        match user_pref['price']:
+            case 10:
+                if(phone_data['price']<=10000):
+                    pass
+                else:
+                    continue
+            case 15:
+                if(phone_data['price']<=15000):
+                    pass
+                else:
+                    continue
+            case 20:
+                if(phone_data['price']<=20000):
+                    pass
+                else:
+                    continue
+            case 30:
+                if(phone_data['price']<=30000):
+                    pass
+                else:
+                    continue
+            case 31:
+                if(phone_data['price']>30000):
+                    pass
+                else:
+                    continue
+        if (user_pref['disp']>phone_data['disp']):
+            disp=abs(user_pref['disp']-phone_data['disp'])
+            sup=0
+        else:
+            disp=0
+            sup+=phone_data['disp']-user_pref['disp']       #supplementary value
+            
+        if (user_pref['gaming']>phone_data['gaming']):
+            gaming=abs(user_pref['gaming']-phone_data['gaming'])
+        else:
+            gaming=0
+            sup+= phone_data['gaming']-user_pref['gaming']
+            
+        if (user_pref['batt']>phone_data['batt']):
+            batt=abs(phone_data['batt']-user_pref['batt'])
+        else:
+            batt=0
+            sup+= phone_data['batt']-user_pref['batt']
+        
+        if (user_pref['cam']>phone_data['cam']):
+            cam=abs(user_pref['cam']-phone_data['cam'])
+        else:
+            cam=0
+            sup= phone_data['cam']-user_pref['cam']
+        
+        agg=disp+gaming+batt+cam            #adding the all values to get aggregate
+       #adding values to the dataframe
+        pagg=pagg.append({        
+            'gaming':phone_data['gaming'],
+            'batt':phone_data['batt'],
+            'disp':phone_data['disp'],
+            'cam':phone_data['cam'],
+            'name':phone_data['name'],
+            'price':phone_data['price'],
+            'agg':agg,
+            'sup':sup
+            },ignore_index=True)
+    
+    pagg=pagg.sort_values(by=['agg'],ascending=True)
+    if(len(pagg.loc[pagg['agg']==pagg['agg'].iloc[0]])==1):
+        print("hello")
+        winner=pagg.iloc[0].to_dict()
+    else:
+        pf=pagg.loc[pagg['agg']==pagg['agg'].iloc[0]]
+        pf=pf.sort_values(by=['sup'],ascending=False)
+        winner=pf.iloc[0].to_dict()
+    print(winner)
+    return winner
